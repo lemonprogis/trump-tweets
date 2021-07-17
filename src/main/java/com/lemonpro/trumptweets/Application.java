a3f2a6b70f9baf40cd43a20fb835ed3ce307913e
Ed Briggler <ed.briggler@jbhunt.com>
2021-07-17 16:45:22 -0500

    upgrade dependencies.

--- src/main/java/com/lemonpro/trumptweets/Application.java
+++ src/main/java/com/lemonpro/trumptweets/Application.java
@@ -2,14 +2,12 @@ package com.lemonpro.trumptweets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.lemonpro.trumptweets")
-public class TrumpTweetsApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrumpTweetsApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
